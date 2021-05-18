5530f383b5498c63d55f50f44290ad14b86725a0
Mariel Alaba <mariel.alaba@students.fhnw.ch>
2021-05-18 11:33:19 +0200

    add text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,14 +5,8 @@ import { Heading1 } from "../typography/heading1";
 export default function Home() {
   return (
     <div>
-      <Heading1>Welcome to NSW</Heading1>
-      <Image
-        src="images/centurion.jpg"
-        alt="Centurion"
-        height="h-72"
-        className="my-8"
-      />
-      <LinkButton href="/angebote">Button</LinkButton>
+      <Heading1>Welcome to NSW - next try</Heading1>
+      <p> text</p>
     </div>
   );
 }
