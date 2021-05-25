d6df37e3885bed203d8e74364812ba3765e070c8
Nadia Posch <nadia@smartive.ch>
2021-05-25 10:47:15 +0200

    add contact form

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -1,9 +1,27 @@
+import { Button } from "../elements/button";
 import { Input } from "../elements/input";
 import { Label } from "../elements/label";
 
 export const ContactForm = () => (
-  <form className="flex flex-col" action="">
-    <Label forInput="firma">Firma</Label>
-    <Input id="firma" placeholder="Firma" />
+  <form
+    className="max-w-full lg:max-w-2xl grid grid-cols-1 lg:grid-cols-2 gap-x-12 gap-y-8 mb-10"
+    action=""
+  >
+    <Input id="firma" label="Firma*" />
+    <Input id="name" label="Name*" />
+    <Input id="email" label="E-Mail*" />
+    <Input id="tel" label="Telefonnummer*" />
+    <Input id="betreff" label="Betreff" className="lg:col-span-2" />
+    <div className="flex flex-col lg:col-span-2">
+      <Label forInput="nachricht">Nachricht</Label>
+      <textarea id="nachricht" className="h-32 border rounded-md" />
+    </div>
+    <Button
+      onClick={() => alert("submitted")}
+      type="submit"
+      className="lg:col-span-2 mx-auto"
+    >
+      senden
+    </Button>
   </form>
 );
--- src/elements/input.tsx
+++ src/elements/input.tsx
@@ -1,12 +1,26 @@
 import React, { FC } from "react";
+import { Label } from "./label";
 
 type Props = {
   id: string;
+  className?: string;
   placeholder?: string;
+  label?: string;
 };
 
-export const Input: FC<Props> = ({ children, id, placeholder = "" }) => (
-  <input type="text" id={id} placeholder={placeholder} className="border p-4">
-    {children}
-  </input>
+export const Input: FC<Props> = ({
+  id,
+  className = "",
+  placeholder = "",
+  label = "",
+}) => (
+  <div className={`flex flex-col w-full ${className}`}>
+    {label && <Label forInput={id}>{label}</Label>}
+    <input
+      type="text"
+      id={id}
+      placeholder={placeholder}
+      className="border p-3 rounded-md focus:border-gold outline-none"
+    />
+  </div>
 );
