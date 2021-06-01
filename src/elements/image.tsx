ceb08c2abd521d57b5a345a9c89cbf3c80c6ed5e
Nadia Posch <nadia@smartive.ch>
2021-06-01 10:22:17 +0200

    add image component

---
+++ src/elements/image.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from "react";
+
+type Props = {
+  src: string;
+  alt: string;
+  width?: string;
+  height?: string;
+  className?: string;
+};
+
+export const Image: FC<Props> = ({
+  src,
+  alt,
+  width = "",
+  height = "",
+  className = "",
+}) => (
+  <img
+    src={src}
+    alt={alt}
+    className={`${width ? width : "max-w-full"} ${
+      height ? height : "h-auto"
+    } ${className}`}
+  />
+);
