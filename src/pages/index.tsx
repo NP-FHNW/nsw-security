f7a83f3a60d19c32bec80aaa3764a49f5b5dc039
Orell Bühler <17276101+OrellBuehler@users.noreply.github.com>
2021-06-08 11:09:32 +0200

    Fix #10

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,68 +1,57 @@
-import { IconCardSmall } from "../components/icon-card-small";
 import { ImageCard } from "../components/image-card";
-import { ImageTeaser } from "../components/image-teaser";
-import { StarIcon } from "../elements/icons/star-icon";
 import { LinkButton } from "../elements/link-button";
 import { Heading1 } from "../typography/heading1";
-import { Heading2 } from "../typography/heading2";
-import { Heading3 } from "../typography/heading3";
-import { Paragraph } from "../typography/paragraph";
+import { HiStar, HiUsers, HiOutlineCube } from "react-icons/hi";
+import {IconCardSmall} from "../components/icon-card-small";
 
 export default function Home() {
   return (
-    <div>
-      <Heading1>Welcome to NSW</Heading1>
-
-      <Heading2>IMAGE CARD</Heading2>
-      <ImageCard
-        imgSrc="/images/centurion.jpg"
-        title="Title"
-        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed purus enim, mollis ac dictum in, pellentesque a ex. Ut nibh urna, lacinia eget malesuada in, posuere ac odio. Curabitur lacus nulla, egestas non aliquam vitae, convallis id elit. Vivamus efficitur a mauris non bibendum. Donec auctor semper neque."
-      >
-        <LinkButton href="/angebote" className="mt-4">
-          Zu den Angeboten
-        </LinkButton>
-      </ImageCard>
-
-      <Heading2>ICON CARD</Heading2>
-      <IconCardSmall
-        icon={<StarIcon width={48} height={48} />}
-        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-        link="/faq"
-        linkLabel="FAQ"
-      />
-
-      <Heading2>IMAGE TEASER</Heading2>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-        <ImageTeaser imgSrc="/images/centurion.jpg">
-          <Heading3>Teaser1</Heading3>
-          <Paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed purus
-            enim, mollis ac dictum in
-          </Paragraph>
-        </ImageTeaser>
-        <ImageTeaser imgSrc="/images/centurion.jpg">
-          <Heading3>Teaser2</Heading3>
-          <Paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed purus
-            enim, mollis ac dictum in
-          </Paragraph>
-        </ImageTeaser>
-        <ImageTeaser imgSrc="/images/centurion.jpg">
-          <Heading3>Teaser3</Heading3>
-          <Paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed purus
-            enim, mollis ac dictum in
-          </Paragraph>
-        </ImageTeaser>
-        <ImageTeaser imgSrc="/images/centurion.jpg">
-          <Heading3>Teaser4</Heading3>
-          <Paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed purus
-            enim, mollis ac dictum in
-          </Paragraph>
-        </ImageTeaser>
-      </div>
+    <div className="grid grid-rows gap-10 items-center">
+        <Heading1>IHRE SICHERHEIT INTERESSIERT UNS MIT SICHERHEIT WIR MACHEN UNS STARK FÜR SIE</Heading1>
+        <div className="grid grid-rows gap-12">
+            <ImageCard
+                imgSrc={"https://www.nsw.ch/wp-content/uploads/2018/06/n-medium2x-16x9-300x179.jpg"}
+                title={"Centurion Baustelle in Windisch."}
+                text={"Während des Neubau des Centurion Tower in Windisch haben wir für Sicherheit gesorgt."}
+                imgDescription={"Centurion Baustelle Windisch"}>
+                <LinkButton href={"/Angebote"}>Angebote</LinkButton>
+            </ImageCard>
+            <ImageCard
+                imgSrc={"https://www.nsw.ch/wp-content/uploads/2018/07/IMG_0005a-300x225.jpg"}
+                title={"Neuwiesen Center Winterthur."}
+                text={"Mit diesen Angeboten machen wir uns für Sie stark."}
+                imgDescription={"Neuwiesen Center Winterthur"}
+                reverse={true}>
+                <LinkButton href={"/Angebote"}>Angebote</LinkButton>
+            </ImageCard>
+        </div>
+        <div className="grid grid-cols-3 gap-8 items-center">
+            <IconCardSmall
+                icon={<HiStar size={180} className="bg-gray-300 rounded-full p-2"/>}
+                text={"Gross- und Kleinanlässe"}
+                link={"/Angebote"}
+                linkLabel={"Details"}/>
+            <IconCardSmall
+                icon={<HiUsers size={180} className="bg-gray-300 rounded-full p-2"/>}
+                text={"Begleit- und Personenschutz"}
+                link={"/Angebote"}
+                linkLabel={"Details"}/>
+            <IconCardSmall
+                icon={<HiOutlineCube size={180} className="bg-gray-300 rounded-full p-2"/>}
+                text={"Baustellenservice"}
+                link={"/Angebote"}
+                linkLabel={"Details"}/>
+        </div>
+        <div className="inline-grid">
+            <div className="flex flex-col items-center gap-4">
+                <h2 className="font-bold">Fragen?</h2>
+                <p>Gerne beantworten wir alle Ihre Fragen:</p>
+                <div className="grid grid-cols-2 gap-8">
+                    <LinkButton href={"/faq"}>FAQ</LinkButton>
+                    <LinkButton href={"/kontakt"}>Kontakt</LinkButton>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
