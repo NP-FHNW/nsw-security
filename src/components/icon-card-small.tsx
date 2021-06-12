50a1904e4aba2a326f05d91d80c42895fd7bc463
Orell Bühler <17276101+OrellBuehler@users.noreply.github.com>
2021-06-12 18:01:35 +0200

    Updated icon-card-small responsiveness

--- src/components/icon-card-small.tsx
+++ src/components/icon-card-small.tsx
@@ -6,11 +6,12 @@ type Props = {
   text: string;
   link: string;
   linkLabel: string;
+  className?: string;
 };
 
-export const IconCardSmall: FC<Props> = ({ icon, text, link, linkLabel }) => (
-  <div className="inline-grid rounded border-2 border-gray-300 p-6">
-    <div className="flex flex-col items-center gap-6">
+export const IconCardSmall: FC<Props> = ({ icon, text, link, linkLabel , className}) => (
+  <div className={`inline-grid rounded border-2 border-gray-300 p-6 ${className}`}>
+    <div className="flex flex-col items-center gap-6  m-auto">
       <div className="flex-auto">{icon}</div>
       <div className="flex-auto">
         <h4>{text}</h4>
