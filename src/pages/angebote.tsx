0593d6ef694df16615b2480303c125629c4b128f
Nadia Posch <nadia@smartive.ch>
2021-06-14 09:21:11 +0200

    styling fixes

--- src/pages/angebote.tsx
+++ src/pages/angebote.tsx
@@ -1,44 +1,71 @@
+import { HiOutlineCube, HiStar, HiUsers } from "react-icons/hi";
+import { IconCardBig } from "../components/icon-card-big";
+import { LinkButton } from "../elements/link-button";
 import { Heading1 } from "../typography/heading1";
-import {Heading2} from "../typography/heading2";
-import {IconCardBig} from "../components/icon-card-big";
-import { HiStar, HiUsers, HiOutlineCube } from "react-icons/hi";
-import {LinkButton} from "../elements/link-button";
-import {Heading3} from "../typography/heading3";
-
+import { Heading3 } from "../typography/heading3";
 
 export default function Angebote() {
   return (
     <div className="grid gap-10">
-      <Heading2>Lassen Sie sich von unseren Angeboten überzeugen</Heading2>
-        <div className="grid gap-16">
-            <IconCardBig
-                icon={<HiStar className="w-20 sm:w-30 md:w-40 lg:w-50 h-full bg-gray-200 rounded-full p-2"/>}
-                button={<LinkButton href={"/kontakt"}>Offerte anfragen</LinkButton>}>
-                <Heading3>Grossanlässe</Heading3>
-                <p>
-                    Die NSW spezialisierte sich in den letzten Jahren vor allem auf Grossanlässe. Als Sicherheitsverantwortliche der ZSC - Lions verfügen wir in diesem Bereich über grosse Erfahrung. Immer wieder geraten Grossveranstaltungen aufgrund von Ausschreitungen oder sonstigen negativen Ereignissen in die Schlagzeilen. Um dies zu vermeiden, braucht es ein umfangreiches Sicherheitskonzept welches wir mit unseren Kunden gemeinsam ausarbeiten, damit ihre Veranstaltung ein voller Erfolg wird.
-                </p>
-            </IconCardBig>
-            <IconCardBig
-                icon={<HiUsers className="w-20 sm:w-30 md:w-40 lg:w-50 h-full bg-gray-200 rounded-full p-2"/>}
-                button={<LinkButton href={"/kontakt"}>Offerte anfragen</LinkButton>}>
-                <Heading3>Kleinanlässe</Heading3>
-                <p>
-                    Kleinanlässe sind für uns Veranstaltungen mit einer Besucherzahl von bis zu 2000 Personen. Es ist nicht notwendig, ein gleich grosses Sicherheitsmanagement zu erstellen wie für Grossanlässe. In einem persönlichen Gespräch klären wir mit ihnen das Notwendige, damit Sie auch mit kleinem Budget eine maximale Sicherheitsleistung im Namen ihrer Veranstaltung bieten können. An Klein- und Grossanlässen umfasst das Einsatzgebiet der NSW Security - Services folgende Gebiete:
-                    - Zutritts- und Eingangskontrollen - VIP- und Backstagesicherheit
-                    - Personenschutz
-                    - Innendienst
-                    - Bühnengraben
-                    - Objektschutz                </p>
-            </IconCardBig>
-            <IconCardBig
-                icon={<HiOutlineCube className="w-20 sm:w-30 md:w-40 lg:w-50 h-full bg-gray-200 rounded-full p-2"/>}
-                button={<LinkButton href={"/kontakt"}>Offerte anfragen</LinkButton>}>
-                <Heading3>Baustellenservice</Heading3>
-                <p>
-                    Die NSW spezialisierte sich in den letzten Jahren vor allem auf Grossanlässe. Als Sicherheitsverantwortliche der ZSC - Lions verfügen wir in diesem Bereich über grosse Erfahrung. Immer wieder geraten Grossveranstaltungen aufgrund von Ausschreitungen oder sonstigen negativen Ereignissen in die Schlagzeilen. Um dies zu vermeiden, braucht es ein umfangreiches Sicherheitskonzept welches wir mit unseren Kunden gemeinsam ausarbeiten, damit ihre Veranstaltung ein voller Erfolg wird.                </p>
-            </IconCardBig>
-        </div>
+      <Heading1>MIT DIESEN ANGEBOTEN MACHEN WIR UNS FÜR SIE STARK</Heading1>
+      <div className="grid gap-16">
+        <IconCardBig
+          icon={
+            <HiStar className="w-20 sm:w-30 md:w-40 lg:w-50 h-full bg-gray-200 rounded-full p-2" />
+          }
+          button={<LinkButton href={"/kontakt"}>Offerte anfragen</LinkButton>}
+        >
+          <Heading3>Grossanlässe</Heading3>
+          <p>
+            Die NSW spezialisierte sich in den letzten Jahren vor allem auf
+            Grossanlässe. Als Sicherheitsverantwortliche der ZSC - Lions
+            verfügen wir in diesem Bereich über grosse Erfahrung. Immer wieder
+            geraten Grossveranstaltungen aufgrund von Ausschreitungen oder
+            sonstigen negativen Ereignissen in die Schlagzeilen. Um dies zu
+            vermeiden, braucht es ein umfangreiches Sicherheitskonzept welches
+            wir mit unseren Kunden gemeinsam ausarbeiten, damit ihre
+            Veranstaltung ein voller Erfolg wird.
+          </p>
+        </IconCardBig>
+        <IconCardBig
+          icon={
+            <HiUsers className="w-20 sm:w-30 md:w-40 lg:w-50 h-full bg-gray-200 rounded-full p-2" />
+          }
+          button={<LinkButton href={"/kontakt"}>Offerte anfragen</LinkButton>}
+        >
+          <Heading3>Kleinanlässe</Heading3>
+          <p>
+            Kleinanlässe sind für uns Veranstaltungen mit einer Besucherzahl von
+            bis zu 2000 Personen. Es ist nicht notwendig, ein gleich grosses
+            Sicherheitsmanagement zu erstellen wie für Grossanlässe. In einem
+            persönlichen Gespräch klären wir mit ihnen das Notwendige, damit Sie
+            auch mit kleinem Budget eine maximale Sicherheitsleistung im Namen
+            ihrer Veranstaltung bieten können. An Klein- und Grossanlässen
+            umfasst das Einsatzgebiet der NSW Security - Services folgende
+            Gebiete: - Zutritts- und Eingangskontrollen - VIP- und
+            Backstagesicherheit - Personenschutz - Innendienst - Bühnengraben -
+            Objektschutz{" "}
+          </p>
+        </IconCardBig>
+        <IconCardBig
+          icon={
+            <HiOutlineCube className="w-20 sm:w-30 md:w-40 lg:w-50 h-full bg-gray-200 rounded-full p-2" />
+          }
+          button={<LinkButton href={"/kontakt"}>Offerte anfragen</LinkButton>}
+        >
+          <Heading3>Baustellenservice</Heading3>
+          <p>
+            Die NSW spezialisierte sich in den letzten Jahren vor allem auf
+            Grossanlässe. Als Sicherheitsverantwortliche der ZSC - Lions
+            verfügen wir in diesem Bereich über grosse Erfahrung. Immer wieder
+            geraten Grossveranstaltungen aufgrund von Ausschreitungen oder
+            sonstigen negativen Ereignissen in die Schlagzeilen. Um dies zu
+            vermeiden, braucht es ein umfangreiches Sicherheitskonzept welches
+            wir mit unseren Kunden gemeinsam ausarbeiten, damit ihre
+            Veranstaltung ein voller Erfolg wird.{" "}
+          </p>
+        </IconCardBig>
+      </div>
     </div>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,60 +1,81 @@
+import { HiOutlineCube, HiStar, HiUsers } from "react-icons/hi";
+import { IconCardSmall } from "../components/icon-card-small";
 import { ImageCard } from "../components/image-card";
 import { LinkButton } from "../elements/link-button";
 import { Heading1 } from "../typography/heading1";
-import { HiStar, HiUsers, HiOutlineCube } from "react-icons/hi";
-import {IconCardSmall} from "../components/icon-card-small";
+import { Heading2 } from "../typography/heading2";
+import { Paragraph } from "../typography/paragraph";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows gap-8 md:gap-18 lg:gap-28 items-center">
-        <Heading1>IHRE SICHERHEIT INTERESSIERT UNS MIT SICHERHEIT WIR MACHEN UNS STARK FÜR SIE</Heading1>
+    <>
+      <Heading1>IHRE SICHERHEIT INTERESSIERT UNS MIT SICHERHEIT</Heading1>
+      <div className="grid grid-rows mt-16 gap-8 md:gap-16 lg:gap-20 items-center">
         <div className="grid grid-rows gap-8 md:gap-16 lg:gap-20">
-            <ImageCard
-                imgSrc={"/images/centurion.jpg"}
-                title={"Centurion Baustelle in Windisch."}
-                text={"Während des Neubau des Centurion Tower in Windisch haben wir für Sicherheit gesorgt."}
-                imgDescription={"Centurion Baustelle Windisch"}>
-                <LinkButton className="my-4" href={"/Angebote"}>Angebote</LinkButton>
-            </ImageCard>
-            <ImageCard
-                imgSrc={"/images/neuwiesen-center-winti.jpg"}
-                title={"Neuwiesen Center Winterthur."}
-                text={"Mit diesen Angeboten machen wir uns für Sie stark."}
-                imgDescription={"Neuwiesen Center Winterthur"}
-                reverse={true}>
-                <LinkButton className="my-4" href={"/Angebote"}>Angebote</LinkButton>
-            </ImageCard>
+          <ImageCard
+            imgSrc={"/images/centurion.jpg"}
+            title={"Centurion Baustelle in Windisch."}
+            text={
+              "Während des Neubau des Centurion Tower in Windisch haben wir für Sicherheit gesorgt."
+            }
+            imgDescription={"Centurion Baustelle Windisch"}
+          >
+            <LinkButton className="my-4" href={"/Angebote"}>
+              Angebote
+            </LinkButton>
+          </ImageCard>
+          <ImageCard
+            imgSrc={"/images/neuwiesen-center-winti.jpg"}
+            title={"Neuwiesen Center Winterthur."}
+            text={"Mit diesen Angeboten machen wir uns für Sie stark."}
+            imgDescription={"Neuwiesen Center Winterthur"}
+            reverse={true}
+          >
+            <LinkButton className="my-4" href={"/Angebote"}>
+              Angebote
+            </LinkButton>
+          </ImageCard>
         </div>
+
         <div className="grid grid-cols-1 grid-rows-3 md:grid-cols-3 md:grid-rows-1 lg:grid-cols-3 lg:grid-rows-1 gap-8 md:gap-12 lg:gap-16 items-center">
-            <IconCardSmall
-                className="h-full"
-                icon={<HiStar className="w-20 sm:w-30 md:w-35 lg:w-40 h-full bg-gray-200 rounded-full p-2"/>}
-                text={"Gross- und Kleinanlässe"}
-                link={"/Angebote"}
-                linkLabel={"Details"}/>
-            <IconCardSmall
-                className="h-full"
-                icon={<HiUsers className="w-20 sm:w-30 md:w-35 lg:w-40 h-full bg-gray-200 rounded-full p-2"/>}
-                text={"Begleit- und Personenschutz"}
-                link={"/Angebote"}
-                linkLabel={"Details"}/>
-            <IconCardSmall
-                className="h-full"
-                icon={<HiOutlineCube className="w-20 sm:w-30 md:w-35 lg:w-40 h-full bg-gray-200 rounded-full p-2"/>}
-                text={"Baustellenservice"}
-                link={"/Angebote"}
-                linkLabel={"Details"}/>
+          <IconCardSmall
+            className="h-full"
+            icon={
+              <HiStar className="w-20 sm:w-30 md:w-35 lg:w-40 h-full bg-gray-200 rounded-full p-2" />
+            }
+            text={"Gross- und Kleinanlässe"}
+            link={"/Angebote"}
+            linkLabel={"Details"}
+          />
+          <IconCardSmall
+            className="h-full"
+            icon={
+              <HiUsers className="w-20 sm:w-30 md:w-35 lg:w-40 h-full bg-gray-200 rounded-full p-2" />
+            }
+            text={"Begleit- und Personenschutz"}
+            link={"/Angebote"}
+            linkLabel={"Details"}
+          />
+          <IconCardSmall
+            className="h-full"
+            icon={
+              <HiOutlineCube className="w-20 sm:w-30 md:w-35 lg:w-40 h-full bg-gray-200 rounded-full p-2" />
+            }
+            text={"Baustellenservice"}
+            link={"/Angebote"}
+            linkLabel={"Details"}
+          />
         </div>
         <div className="inline-grid">
-            <div className="flex flex-col items-center gap-4">
-                <h2 className="font-bold">Fragen?</h2>
-                <p>Gerne beantworten wir alle Ihre Fragen:</p>
-                <div className="grid grid-cols-2 gap-8">
-                    <LinkButton className="text-center" href={"/faq"}>FAQ</LinkButton>
-                    <LinkButton className="text-center" href={"/kontakt"}>Kontakt</LinkButton>
-                </div>
-            </div>
+          <div className="flex flex-col items-center gap-4">
+            <Heading2>Fragen?</Heading2>
+            <Paragraph>Zögern Sie nicht uns zu kontaktieren</Paragraph>
+            <LinkButton className="text-center" href={"/kontakt"}>
+              Kontakt
+            </LinkButton>
+          </div>
         </div>
-    </div>
+      </div>
+    </>
   );
 }
