c4707e352c56db081c56750bc9c3bab50f61b00e
Mariel Alaba <mariel.alaba@students.fhnw.ch>
2021-06-15 08:05:55 +0200

    added references

--- src/pages/referenzen.tsx
+++ src/pages/referenzen.tsx
@@ -6,33 +6,63 @@ export default function Referenzen() {
     <>
       <Heading1>UNSERE REFERENZEN SIND GOLD WERT</Heading1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-16 lg:gap-20">
-        <ReferenceCard
-          imgSrc={"/images/centurion.jpg"}
-          title={"Centurion Baustelle"}
-          link="https://centurion-towerhotel.ch/"
+
+      <ReferenceCard
+          imgSrc={"/images/zsc-logo.jpg"}
+          title={"ZSC Lions (seit 2001)"}
+          link="https://www.zsclions.ch/"
         ></ReferenceCard>
-        <ReferenceCard
-          imgSrc={"/images/neuwiesen-center-winti.jpg"}
-          title={"Neuwiesen Center Winterthur"}
-          link="https://neuwiesen.ch/"
-        />
+
+      <ReferenceCard
+          imgSrc={"/images/gck-logo.png"}
+          title={"GCK  Lions (seit 2001)"}
+          link="https://gck.zsclions.ch/"
+        ></ReferenceCard>
+
         <ReferenceCard
           imgSrc={"/images/swiss-ice-hockey.jpg"}
           title={"Swiss Ice Hockey"}
           link="https://www.sihf.ch/de/"
         />
+
         <ReferenceCard
           imgSrc={"/images/fc-winterthur.png"}
           title={"FC Winterthur"}
           link="https://fcwinterthur.ch/"
           contain
         />
+
         <ReferenceCard
           imgSrc={"/images/handball-schweiz.png"}
           title={"Handball Schweiz"}
           link="https://www.handball.ch/de/"
           contain
         />
+
+        <ReferenceCard
+          imgSrc={"/images/centurion.jpg"}
+          title={"Centurion Baustelle"}
+          link="https://centurion-towerhotel.ch/"
+        ></ReferenceCard>
+
+        <ReferenceCard
+          imgSrc={"/images/neuwiesen-center-winti.jpg"}
+          title={"Neuwiesen Center Winterthur"}
+          link="https://neuwiesen.ch/"
+        />
+        
+        <ReferenceCard
+          imgSrc={"/images/bp-logo.gif"}
+          title={"b+p baurealisation ag"}
+          link="https://www.bp-baurealisation.ch/"
+        ></ReferenceCard>
+
+        <ReferenceCard
+          imgSrc={"/images/oa-frauenfeld.png"}
+          title={"Openair Frauenfeld"}
+          link="https://www.openair-frauenfeld.ch/"
+        ></ReferenceCard>
+        
       </div>
     </>
   );
